410bd4ba1b3c9587493cfc6d76be192568a06a84
tim-soikie <85976318+tim-soikie@users.noreply.github.com>
2021-09-16 20:49:35 +1200

    fixed up second nav + added dummy data

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -6,14 +6,14 @@ function ListItem(props, { dispatch }) {
     <div class="card">
       <div class="card-image">
         <figure class="image is-4by3">
-          <img src={props.photo} alt="Placeholder image"/>
+          <img src={props.data.photo} alt="Placeholder image"/>
         </figure>
       </div>
       <div class="card-content">
         <div class="media">
           <div class="media-content">
-            {props.name ? <p class="title is-4">{props.name}</p> : <p></p>}            
-            <p class="subtitle is-6">{props.species}</p>
+            {props.data.name ? <p class="title is-4">{props.data.name}</p> : <p></p>}            
+            <p class="subtitle is-6">{props.data.species}</p>
           </div>
         </div>
       </div>
--- client/components/Listings.jsx
+++ client/components/Listings.jsx
@@ -4,15 +4,58 @@ import { useState } from 'react'
 
 import Login from './Login'
 import Register from './Register'
-import Nav from './Nav'
 import ListItem from './ListItem'
 
 import { checkAuth } from '../actions/auth'
 
 function Listings ({ auth, dispatch }) {
-  const animalsLost = []
-  const animalsFound = []
-  const [animalsDisplay, setAnimalsDisplay] = useState ([])
+
+  const animalsLost = [
+    {
+      "id": 1000,
+      "name": "Steve",
+      "species": "chipmunk",
+      "photo": "https://i.redd.it/p8bplj0w2j131.jpg",
+      "user_id": 1
+    },
+    {
+      "id": 1001,
+      "name": "Marvin",
+      "species": "dog",
+      "photo": "https://i.imgur.com/KAh9NOq.jpeg",
+      "user_id": 2
+    },
+    {
+      "id": 1002,
+      "name": "Isabelle",
+      "species": "cat",
+      "photo": "https://i.imgflip.com/5110mw.png",
+      "user_id": 3
+    }
+  ]
+
+  const animalsFound = [
+    {
+      "id": 90000,
+      "species": "dog",
+      "photo": "https://static.vets-now.com/uploads/2017/06/my-dog-is-in-pain-320x212.jpg",
+      "user_id": 4
+    },
+    {
+      "id": 90001,
+      "species": "rabbit",
+      "photo": "https://static.givealittle.co.nz/assets/hero/e5d4f064-ba06-4ddd-9fe1-abfd0157eaf1-800",
+      "user_id": 5
+    },
+    {
+      "id": 90002,
+      "species": "sheep",
+      "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFUOJm7-fzSFEwoEQbohtmV6oJmJzERCn4iACPm7yyGBz3ZUH6q-hNkPsz0ZS4ux2DUN0&usqp=CAU",
+      "user_id": 6
+    }
+  ]
+
+  const [animalsDisplay, setAnimalsDisplay] = useState(animalsLost)
   const [speciesInput, setSpeciesInput] = useState('')
 
   const lostButtonHandler = () => {
@@ -44,7 +87,6 @@ function Listings ({ auth, dispatch }) {
   
   return (
     <>
-      <Nav />
       <h1>Currently on the loose</h1>
       <div class="page-options">
         <div class="dropdown is-hoverable">
@@ -60,7 +102,7 @@ function Listings ({ auth, dispatch }) {
           </div>
           <div class="dropdown-menu" id="dropdown-menu" role="menu">
             <div class="dropdown-content">
-              <a onClick={lostButtonHandler} href="#" class="dropdown-item">
+              <a onClick={lostButtonHandler} class="dropdown-item">
                 Lost
               </a>   
               <a onClick={foundButtonHandler} class="dropdown-item">
