46390dfe156e0c7c71227f1254d436b4f356f235
Maximilon Baddeley <maximilon@Maximilons-iMac.local>
2021-09-17 15:19:17 +1200

    add final listing

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
 function ListItem(props, { dispatch }) {
-
   return (
-    <div class="card">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src={props.data.photo} alt="Placeholder image"/>
+    <div className="card card--item">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img
+            className="petImg"
+            src={props.data.photo}
+            alt="Placeholder image"
+          />
         </figure>
       </div>
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            {props.data.name ? <p class="title is-4">{props.data.name}</p> : <p></p>}            
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            {props.data.name ? (
+              <p className="title is-4">{props.data.name}</p>
+            ) : (
+              <p className="title is-4">unknown</p>
+            )}
             <p class="subtitle is-6">{props.data.species}</p>
           </div>
         </div>
