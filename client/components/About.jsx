1e2b170ea9e9a061f445cd80edfd85061f165435
JULIET-01 <86028118+JULIET-01@users.noreply.github.com>
2021-09-17 14:30:47 +1200

    started about

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <h1>
           Welcome to lost and Found
         </h1>
-        <p><strong>We're here to help reunite you with your furry, scaled, and feathered friends</strong></p>
+        <p className="p-text"><strong>We're here to help reunite you with your furry, scaled, and feathered friends</strong></p>
         <p>Losing a pet is losing a loved one, and we want to get your family back together.</p>
         <p>Here you can post family you've lost and found</p>
       </div>
