941c68ad7a5f55294d60d5b082107ba1d6812f24
JULIET-01 <86028118+JULIET-01@users.noreply.github.com>
2021-09-17 14:49:23 +1200

    styling for about page

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function About() {
   return (
     <>
-      <div>
-        <h1>
+      <div className="about-text">
+        <h2>
           Welcome to lost and Found
-        </h1>
-        <p className="p-text"><strong>We're here to help reunite you with your furry, scaled, and feathered friends</strong></p>
+        </h2>
+        <p><strong>We're here to help reunite you with your furry, scaled, and feathered friends</strong></p>
         <p>Losing a pet is losing a loved one, and we want to get your family back together.</p>
         <p>Here you can post family you've lost and found</p>
       </div>
